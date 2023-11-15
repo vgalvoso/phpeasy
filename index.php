25aef63bfe7d7081476abfb5dc3f6f295ea38f71
vgalvoso <vgalvoso@gmail.com>
2023-11-15 15:39:04 +0800

    optimize index.php

--- index.php
+++ index.php
@@ -3,10 +3,7 @@ define('BASE_DIR',__DIR__);
 require_once "Core/Config.php";
 require_once "Core/Helper.php";
 require_once "Core/Validator.php";
-$file = "vendor/autoload.php";
-if (file_exists($file)) {
-    include $file;
-}
+@$file = include "vendor/autoload.php";
 require_once "autoload.php";
 
 if(!view())
